eaf947604dd293adf145aa21698e95b5280b0588
Andrew <aschoch10@gmail.com>
2022-02-10 09:05:15 -0800

    session manipulation work

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import random
 from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
 app.secret_key = 'super, super secret key name'
@@ -5,13 +6,28 @@ app.secret_key = 'super, super secret key name'
 
 @app.route('/')
 def home():
+    if 'targetNumber' not in session:
+        session ['targetNumber'] = random.randint(1,100)
+    if 'count' not in session:
+        session['count'] = 0
+    else:
+        session['count'] += 1
+
     return render_template("index.html")
 
 @app.route('/submit', methods=['POST'])
 def create_user():
-    print("Succesful post")
-    print("post info: ", request.form)
-    session['number'] = request.form['number']
+    # print("Succesful post")
+    # print("post info: ", request.form)
+    session['number'] = int(request.form['number'])
+    target = session['targetNumber']
+    print("Target number is:", target)
+    session['count'] = 0
+    return redirect('/')
+
+@app.route('/reset')
+def reset():
+    session.clear()
     return redirect('/')
 
 
